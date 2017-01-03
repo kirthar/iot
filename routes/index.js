2f18ef7b92c96364e02fefa677a53a817a96d3d9
Chris Fuertes Garcia <chris@brandcrumb.com>
2017-01-03 13:36:54 +0100

    Reestructure code

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function (req, res, next) {
-    res.send('' +
-        '<a href="/allon"><button>ON !!!</button></a>' +
-        '</br>' +
-        '<a href="/alloff"><button>OFF</button></a>')
-});
